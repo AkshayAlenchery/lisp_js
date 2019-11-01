2c1b1a7afca630c4ffc201019283e31f66460f60
Akshay Alenchery <akshayalenchery@gmail.com>
2019-11-01 08:48:59 +0530

    Implemented If, Begin and Define

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
-const numberParser = (jsonInput, match = null) => (match = jsonInput.match(/^-?(0|[1-9][0-9]*)(\.[0-9]+)?((e|E)(-|\+)?[0-9]+)?/)) === null ? null : [match[0] * 1, jsonInput.slice(match[0].length).trim()]
-
 const env = {
   '+': input => input.reduce((a, b) => a + b),
   '-': input => input.length === 1 ? input[0] * -1 : input.reduce((a, b) => a - b),
   '*': input => input.reduce((a, b) => a * b),
   '/': input => input.reduce((a, b) => a / b),
-  '>': input => input.reduce((a, b) => a > b)
+  '>': input => input.reduce((a, b) => a > b),
+  '<': input => input.reduce((a, b) => a < b),
+  '>=': input => input.reduce((a, b) => a >= b),
+  '<=': input => input.reduce((a, b) => a <= b),
+  '=': input => input.reduce((a, b) => a === b),
+  'pi': Math.PI
+}
+
+const numberParser = (jsonInput, match = null) => (match = jsonInput.match(/^-?(0|[1-9][0-9]*)(\.[0-9]+)?((e|E)(-|\+)?[0-9]+)?/)) === null ? null : [match[0] * 1, jsonInput.slice(match[0].length).trim()]
+
+const variableParser = input => {
+  const variable = input.slice(0, input.indexOf(' ')).trim()
+  if (!variable.match(/^[a-zA-Z]*$/)) return null
+  return [variable, input.slice(variable.length).trim()]
+}
+
+const findFromEnv = input => (env[input] === undefined) ? null : env[input]
+
+const skipParser = inputExp => {
+
+}
+
+const beginParser = inputExp => {
+  if (!inputExp.startsWith('begin')) return null
+  inputExp = inputExp.slice(5).trim()
+  let result
+  while (inputExp[0] !== ')') {
+    result = lispParser(inputExp)
+    if (!result) return null
+    inputExp = result[1].trim()
+  }
+  if (!inputExp.startsWith(')')) return null
+  return result
+}
+
+const defineParser = inputExp => {
+  if (!inputExp.startsWith('define')) return null
+  inputExp = inputExp.slice(6).trim()
+  const varParseResult = variableParser(inputExp)
+  if (!varParseResult) return null
+  const variable = varParseResult[0]
+  inputExp = varParseResult[1].trim()
+  const expResult = lispParser(inputExp)
+  if (!expResult) return null
+  const value = expResult[0]
+  if (!expResult[1].trim().startsWith(')')) return null
+  inputExp = expResult[1].slice(1).trim()
+  env[variable] = value
+  return ['', inputExp]
+}
+
+const ifParser = inputExp => {
+  if (!inputExp.startsWith('if')) return null
+  inputExp = inputExp.slice(2).trim()
+  const result = lispParser(inputExp)
+  inputExp = result[1].trim()
+  if (result[0]) return lispParser(inputExp)
+  inputExp = inputExp.slice(inputExp.indexOf(')') + 1).trim()
+  if (inputExp[0] === '(') return lispParser(inputExp)
+  return ['', inputExp]
 }
 
 const operatorParser = inputExp => {
-  if (!inputExp.startsWith('(')) return null
-  inputExp = inputExp.slice(1).trim()
   const op = inputExp[0]
   const operands = []
   if (!env[op]) return null
@@ -24,10 +79,21 @@ const operatorParser = inputExp => {
   return [env[op](operands), inputExp.slice(1).trim()]
 }
 
-const lispParser = input => {
-  input.trim()
-  const result = operatorParser(input) || numberParser(input)
+const mainParsers = input => {
+  if (!input.startsWith('(')) return null
+  input = input.slice(1).trim()
+  const result = beginParser(input) || defineParser(input) || ifParser(input) || operatorParser(input)
   return result
 }
 
-console.log(lispParser('(+ 2 3 (* 4 5 (/ 6 3)) (- 4 2))')[0])
+const lispParser = input => {
+  input.trim()
+  let result = mainParsers(input) || numberParser(input)
+  if (result) return result
+  result = variableParser(input)
+  if (result === null) return null
+  return [findFromEnv(result[0]), result[1].trim()]
+}
+
+console.log(lispParser('( begin ( define x 12 ) ( define y 1 ) ( if ( < x y ) ( + ( + x y ) ( - x y ) ) ( * x y ) ) )'))
+console.log(env)
